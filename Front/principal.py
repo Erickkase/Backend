fb672d906f20306ac7235e901dc55a115d268a90
Allan <allancorrea2002@outlook.es>
2024-07-04 13:17:00 -0500

    HTML y CSS
    Inicio_Sesion actualizado

--- Front/principal.py
+++ Front/principal.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("inicio_de_sesion.html")
+    return render_template("")
 
-@app.route('/Inicio')
-def Inicio():
-    return 'Pagina Inicio'
+@app.route('/InicioSesion')
+def InicioSesion():
+    return render_template("")

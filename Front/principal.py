5414317de47966f9a00d462fd292506584a6cbc8
Duvard <98781491+Duvard1@users.noreply.github.com>
2024-07-08 17:19:12 -0500

    commit front flask - #2.1

--- Front/principal.py
+++ Front/principal.py
@@ -5,9 +5,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template("")
+def PaginaInicial():
+    return render_template("pagina_inicial.html")
 
 @app.route('/InicioSesion')
 def InicioSesion():
-    return render_template("")
+    return render_template("inicio_sesion.html")
+
+@app.route('/Registrarse')
+def Registrarse():
+    return render_template("registrarse.html")

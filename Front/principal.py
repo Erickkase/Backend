33329219730033d5cba64a9805da2ff998bbb924
Allan <allancorrea2002@outlook.es>
2024-07-09 22:14:13 -0500

    Funcionalidad HTML
    Funcionalidad de los distintos botones hacia las otras páginas

--- Front/principal.py
+++ Front/principal.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 #Creamos aplicacion Flask 
 app = Flask(__name__)
 
+@app.route('/PaginaInicial')#para poder referenciar en los botones de retorno(atras)
 @app.route('/')
 def PaginaInicial():
     return render_template("pagina_inicial.html")
@@ -15,3 +16,6 @@ def InicioSesion():
 @app.route('/Registrarse')
 def Registrarse():
     return render_template("registrarse.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=6020)

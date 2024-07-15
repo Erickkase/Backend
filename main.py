12e335a93394046f2acf577cb8e4c3024541da10
Erickkase <ehbonilla@uce.edu.ec>
2024-07-14 20:56:39 -0500

    Se añadio de nuevo la funcionalidad CORS para la comunicacion en el localhost

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from routers import comentarios
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#CORS
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Routers
 app.include_router(comentarios.router)
